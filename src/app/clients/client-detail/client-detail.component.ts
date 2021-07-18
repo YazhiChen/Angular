239dee41cf43c2a820eee0fa3179ef1027860ce2
Yazhi Chen <yche0338@student.monash.edu>
2021-07-19 08:18:05 +1000

    frontEndrevise

--- src/app/clients/client-detail/client-detail.component.ts
+++ src/app/clients/client-detail/client-detail.component.ts
@@ -32,7 +32,7 @@ export class ClientDetailComponent implements OnInit {
 
   onSubmit(form: NgForm) {
     // insert record
-    if (this.service.formData.ClientID == 0) {
+    if (this.service.formData.ClientID == 0 && !this.service.formData.Name.includes(form.name)) {
       this.service.postClientDetail().subscribe(
         res => {
           this.resetForm(form);
